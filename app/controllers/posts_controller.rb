631c6061c5c242006d408cfe60bef29920e9a2a1
koji520 <kojima.kazushi06@gmail.com>
2025-02-27 11:33:18 +0900

    Gemfileにrack-corsとdartsass-railsを追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,6 +3,11 @@ class PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def all
+    @posts = Post.all
+    render json: @posts
+  end
+
   def new
     @post = Post.new
   end
